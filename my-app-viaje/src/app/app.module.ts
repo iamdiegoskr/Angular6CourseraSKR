47d52578be69189884260ab137a4810aed35e2ca
diego martinez <juandiego337@hotmail.com>
2020-11-12 15:49:47 -0500

    add new component for destination-detail, create a new routes to navigate in the app

--- my-app-viaje/src/app/app.module.ts
+++ my-app-viaje/src/app/app.module.ts
@@ -1,20 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { DestinoViajeComponent } from './destino-viaje/destino-viaje.component';
 import { ListaDestinosComponent } from './lista-destinos/lista-destinos.component';
 import { FooterComponent } from './footer/footer.component';
+import { DestinoDetalleComponent } from './destino-detalle/destino-detalle.component';
+
+
+
+//routes
+const ROUTES: Routes = [
+  { path: '', redirectTo:'home', pathMatch:'full'},
+  { path: 'home', component: ListaDestinosComponent},
+  { path: 'destino-detail', component:DestinoDetalleComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     DestinoViajeComponent,
     ListaDestinosComponent,
-    FooterComponent
+    FooterComponent,
+    DestinoDetalleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
